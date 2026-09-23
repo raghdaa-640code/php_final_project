cbe841c78a69bf6ff24e918416da9dd0954bcf0e
raghdaa <raghdaamohamed@students.du.edu.eg>
2026-09-23 12:48:38 +0300

    Fix routes after merge

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,16 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/',function(){
-    return view('welcome');
-});
-
-// Route::get('/admin/dashboard', function () {
-//     return view('admin.users.admin_dasboard.app');
-// });
-
-Route::prefix('/admin')->name('admin.')->group(function(){
-
-    Route::get('/',HomeController::class)->name('home');
-
-    Route::controller(UserController::class)->group(function(){
-
-        Route::get('/users/create','create')->name('users.create');
-        Route::post('/users/store','store')->name('users.store');
-        Route::delete('/users/{id}',"destroy")->name('users.destroy')->where(['id'=>'[0-9]+']);
-        Route::get('/users/{id}/edit',"edit")->name('users.edit')->where(['id'=>'[0-9]+']);
-        Route::put('/users/{id}','update')->name('users.update')->where(['id'=>'[0-9]+']);
-
-        // Route::get('/students','index')->name('students.index');
-        // Route::get('/students/{id}','show')->name('students.show')->where(['id'=>'[0-9]+']);
-        
-        });
-        
-        Route::controller(BookController::class)->group(function(){
-            Route::get('/dashboard','index')->name('dashboard');
-            Route::delete('/books/{id}',"destroy")->name('books.destroy')->where(['id'=>'[0-9]+']);
-            // Route::get('/books/{id}/edit',"edit")->name('books.edit')->where(['id'=>'[0-9]+']);
-            // Route::put('/books/{id}','update')->name('books.update')->where(['id'=>'[0-9]+']);
-
-    
-    });
-
-});
-=======
 use App\Http\Controllers\Admin\ReportController as AdminReportController;
 use App\Http\Controllers\Admin\ReviewController as AdminReviewController;
 use App\Http\Controllers\User\ReviewController as UserReviewController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('reviews.index');
 });
-
 
 Route::get('/reviews', [UserReviewController::class, 'index'])->name('reviews.index');
 
@@ -62,7 +21,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/reviews/{review}', [UserReviewController::class, 'destroy'])->name('reviews.destroy');
 });
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+Route::prefix('/admin')->name('admin.')->group(function () {
+    Route::get('/', HomeController::class)->name('home');
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users/create', 'create')->name('users.create');
+        Route::post('/users/store', 'store')->name('users.store');
+        Route::delete('/users/{id}', 'destroy')->name('users.destroy')->where(['id' => '[0-9]+']);
+        Route::get('/users/{id}/edit', 'edit')->name('users.edit')->where(['id' => '[0-9]+']);
+        Route::put('/users/{id}', 'update')->name('users.update')->where(['id' => '[0-9]+']);
+    });
+
+    Route::controller(BookController::class)->group(function () {
+        Route::get('/dashboard', 'index')->name('dashboard');
+        Route::delete('/books/{id}', 'destroy')->name('books.destroy')->where(['id' => '[0-9]+']);
+    });
+
     Route::get('/reports', [AdminReportController::class, 'index'])->name('reports.index');
     Route::put('/reports/{report}/reply', [AdminReportController::class, 'reply'])->name('reports.reply');
 
@@ -71,4 +45,3 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::delete('/reviews/{review}', [AdminReviewController::class, 'destroy'])->name('reviews.destroy');
     Route::delete('/reviews', [AdminReviewController::class, 'destroyAll'])->name('reviews.destroyAll');
 });
->>>>>>> Review_Reports
