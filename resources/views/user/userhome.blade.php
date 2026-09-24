40c403d61c2a43f9c5db023975a2b5c7e621eb5f
Salma-2006 <salma06mo@gmail.com>
2026-09-24 15:05:22 +0300

    Enhance user home layout with cards and header

--- resources/views/user/userhome.blade.php
+++ resources/views/user/userhome.blade.php
@@ -3,16 +3,36 @@
 
 <link rel="stylesheet" href="{{ asset('dashboard/assets/CSS/userhome.css') }}">
 
-    <div class="user-home-container">
+<div class="user-home-container">
 
-    <h1 class="welcome-message">أهلاً بك يا {{ auth()->user()->name }} في منصتنا!</h1>
+    <header class="welcome-section">
+        <h1 class="welcome-message">أهلاً بك يا <span>{{ auth()->user()->name }}</span> في منصتنا!</h1>
+    </header>
 
-    <div class="links-section">
+    <div class="cards-grid">
         
-        <a href="{{ route('user.showbooks') }}" class="nav-link">تصفح الكتب</a>
-    <a href="{{ route('user.showmybooks',['id' => auth()->id()]) }}" class="nav-link">كتبي</a>
-        <a href="{{ route('user.profile',['id' => auth()->id()]) }}" class="nav-link">الملف الشخصي</a>
+        <a href="{{ route('user.showbooks') }}" class="home-card">
+            <div class="card-icon"><i class="fa-solid fa-book-open"></i></div>
+            <h3>الكتب المعروضة</h3>
+        </a>
+
+        <a href="{{ route('user.showmybooks', ['id' => auth()->id()]) }}" class="home-card">
+            <div class="card-icon"><i class="fa-solid fa-book"></i></div>
+            <h3>الكتب التي عرضتها</h3>
+        </a>
+        <a href="{{route('reviews.index')}}" class="home-card">
+            <div class="card-icon"><i class="fa-solid fa-comments"></i></div>
+            <h3>الآراء</h3>
+        </a>
+
+        <a href="" class="home-card">
+            <div class="card-icon"><i class="fa-solid fa-triangle-exclamation"></i></div>
+            <h3>الإبلاغ عن مشكلة</h3>
+        </a>
+
     </div>
+
 </div>
+
 
 @endsection
